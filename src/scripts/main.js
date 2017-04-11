e476021bdd0179e0d984e9db0d3a93736468ad2d
StanislavMartynencko <ctys7777@gmail.com>
2017-04-11 22:51:35 +0300

    Отрисовка фона, движение персонажа

---
+++ src/scripts/main.js
@@ -0,0 +1,55 @@
+var loaded = false;
+var KEY_LEFT ="ArrowLeft";
+var KEY_RIGHT ="ArrowRight";
+var KEY_UP ="ArrowUp";
+var KEY_DOWN ="ArrowDown";
+var UP = 0,RIGHT=1,DOWN=2,LEFT=3;
+var canvas, image;
+var GRID_WIDTH = 30;
+var GRID_HEIGHT = 20;
+var man;
+function image(){
+	man = new Man(GRID_WIDTH/2|0,GRID_HEIGHT/2|0);
+	image = new Image();
+	image.src = "assets/sprites.png";
+	image.addEventListener ("load",function(){
+		loaded = true;
+		draw();
+	});
+}
+
+function drow(){
+	if (loaded){			
+		for(var x=0;x<GRID_WIDTH;x++){
+			for (var y=0;y<GRID_HEIGHT;y++){
+				drawSprite(x,y,man.floor);
+			}
+			drawSprite(x,-1,man.wallLeftRight);
+			drawSprite(x,GRID_HEIGHT,man.wallLeftRight);
+		}
+		for(var y=0;y<GRID_HEIGHT;y++){
+			drawSprite(-1,y,man.wallUpDownLeft);
+			drawSprite(GRID_WIDTH,y,man.wallUpDownRight);
+		}	
+		drawSprite(-1,-1,man.wallRightDown);
+		drawSprite(GRID_WIDTH,-1,man.wallDownLeft);
+		drawSprite(-1,GRID_HEIGHT,man.wallUpRight);
+		drawSprite(GRID_WIDTH,GRID_HEIGHT,man.wallLeftUp);
+	}	
+}
+function onKeyPressed(e){
+	switch(e.key){
+		case KEY_LEFT:
+			snake.setDirection(LEFT);
+			break;
+		case KEY_RIGHT:
+			snake.setDirection(RIGHT);
+			break;
+		case KEY_UP:
+			snake.setDirection(UP);
+			break;
+		case KEY_DOWN:
+			snake.setDirection(DOWN);
+			break;
+	}
+}
