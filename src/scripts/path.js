5176f3e31db32b1aef6450a345c2401c8e033baf
StanislavMartynencko <ctys7777@gmail.com>
2017-04-12 15:37:09 +0300

    Исправлены отступы

--- src/scripts/path.js
+++ src/scripts/path.js
@@ -1,31 +1,30 @@
 Mans.prototype.setDirection=function(direction){
-		var newX,newY;
-		switch(direction){
-			case LEFT:
-				newX = -1;
-				newY = 0;
-				break;
-
-			case RIGHT:
-				newX = 1;
-				newY = 0;
-				break;
-			case UP:
-				newX = 0;
-				newY = -1;
-				break;
-			case DOWN:
-				newX = 0;
-				newY = 1;
-				break;
-			default:
-				console.error("Unknow direction" + direction);
-				return;
-		}
-		if (newX +this.directionX ==0 && newY+ this.directionY ==0){
-			return;
-		}
-		this.newDirectionX = newX;
-		this.newDirectionY = newY;
+    var newX,newY;
+    switch(direction){
+        case LEFT:
+            newX = -1;
+            newY = 0;
+            break;
+        case RIGHT:
+            newX = 1;
+            newY = 0;
+            break;
+        case UP:
+            newX = 0;
+            newY = -1;
+            break;
+        case DOWN:
+            newX = 0;
+            newY = 1;
+            break;
+        default:
+    console.error("Unknow direction" + direction);
+        return;
+    }
+    if (newX +this.directionX ==0 && newY+ this.directionY ==0){
+        return;
+    }
+    this.newDirectionX = newX;
+    this.newDirectionY = newY;
 
 }
