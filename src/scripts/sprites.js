3e82bf3da44ef77e4cf8922afdf96c07cb48a938
StanislavMartynencko <ctys7777@gmail.com>
2017-06-01 22:29:09 +0300

    Обновления sprites.js

---
+++ src/scripts/sprites.js
@@ -0,0 +1,18 @@
+(function (){
+
+var sprites = {
+	bodyRightDown : new Sprite(0,0),
+	bodyLeftRight : new Sprite(1,0),
+	bodyDownLeft : new Sprite(2,0),
+	bodyUpDown : new Sprite(0,1),
+	bodyUpRight : new Sprite(0,2),
+	bodyLeftUp : new Sprite(2,2),
+
+	tailUp :new Sprite(3,0),
+	tailDown :new Sprite(3,1),
+	tailLeft :new Sprite(3,2),
+	tailRight :new Sprite(4,2),
+	headUp :new Sprite(5,1)
+}
+
+})()
