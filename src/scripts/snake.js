78d555b3b7ada0b33d20555c0f927a324417592f
StanislavMartynencko <ctys7777@gmail.com>
2017-06-02 00:05:11 +0300

    setDirection

--- src/scripts/snake.js
+++ src/scripts/snake.js
@@ -23,6 +23,29 @@
     Snake.prototype.shrink = function(){
         this.tail=this.tail.next;
     }
-    
+    Snake.prototype.setDirection=function(direction){
+        var newX,newY;
+        switch(direction){
+            case LEFT:
+                newX = -1;
+                newY = 0;
+                break;
+            case RIGHT:
+                newX = 1;
+                newY = 0;
+                break;
+            case UP:
+                newX = 0;
+                newY = -1;
+                break;
+            case DOWN:
+                newX = 0;
+                newY = 1;
+                break;
+            default:
+                console.error("Unknow direction" + direction);
+                return;
+        }
+    }
     window.Snake = Snake;
 })()
