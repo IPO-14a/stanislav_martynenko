0e80572349b458a44debe8525483aa309071ef48
StanislavMartynencko <ctys7777@gmail.com>
2017-06-01 23:20:52 +0300

    Add function Snake

--- src/scripts/snake.js
+++ src/scripts/snake.js
@@ -4,6 +4,14 @@
 		this.y = y;
 		this.next = null;
 	}
-
+	function Snake(x,y) {
+		this.directionX = 0;
+		this.directionY = -1;
+		this.newDirectionX = this.directionX;
+		this.newDirectionY = this.directionY;
+		this.head = new SnakeSegment(x,y);
+		this.tail = new SnakeSegment(x+this.directionX,y-this.directionY);
+		this.tail.next = this.head;
+	}
 	window.Snake = Snake;
 })()
