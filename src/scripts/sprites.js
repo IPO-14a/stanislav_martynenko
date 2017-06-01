c67e2ba727b18160bbc8111517d80dcc64e69140
StanislavMartynencko <ctys7777@gmail.com>
2017-06-01 22:48:48 +0300

    Исправление табуляции

--- src/scripts/sprites.js
+++ src/scripts/sprites.js
@@ -1,36 +1,35 @@
 (function (){
-function Sprite(h,v,width,height){
-	this.h=h;
-	this.v=v;
-	this.width=typeof width =="undefined"?1:width;
-	this.height=typeof height =="undefined"?1:height;
+    function Sprite(h,v,width,height){
+    this.h=h;
+    this.v=v;
+    this.width=typeof width =="undefined"?1:width;
+    this.height=typeof height =="undefined"?1:height;
 }
 
 var sprites = {
-	bodyRightDown : new Sprite(0,0),
-	bodyLeftRight : new Sprite(1,0),
-	bodyDownLeft : new Sprite(2,0),
-	bodyUpDown : new Sprite(0,1),
-	bodyUpRight : new Sprite(0,2),
-	bodyLeftUp : new Sprite(2,2),
-
-	tailUp :new Sprite(3,0),
-	tailDown :new Sprite(3,1),
-	tailLeft :new Sprite(3,2),
-	tailRight :new Sprite(4,2),
-	headUp :new Sprite(5,1),
-	headDown :new Sprite(5,2),
-	headLeft :new Sprite(4,0),
-	headRight :new Sprite(5,0),
-	food : new Sprite(4,1),
-	wallRightDown : new Sprite(0,3),
-	wallUpDownLeft : new Sprite(0,4),
-	wallUpDownRight : new Sprite(2,4), 
-	wallUpRight : new Sprite(0,5),
-	wallLeftRight : new Sprite(1,3),
-	wallDownLeft : new Sprite(2,3),
-	wallLeftUp :new Sprite(2,5),
-	floor : new Sprite(3,4)
+    bodyRightDown : new Sprite(0,0),
+    bodyLeftRight : new Sprite(1,0),
+    bodyDownLeft : new Sprite(2,0),
+    bodyUpDown : new Sprite(0,1),
+    bodyUpRight : new Sprite(0,2),
+    bodyLeftUp : new Sprite(2,2),
+    tailUp :new Sprite(3,0),
+    tailDown :new Sprite(3,1),
+    tailLeft :new Sprite(3,2),
+    tailRight :new Sprite(4,2),
+    headUp :new Sprite(5,1),
+    headDown :new Sprite(5,2),  
+    headLeft :new Sprite(4,0),
+    headRight :new Sprite(5,0),
+    food : new Sprite(4,1), 
+    wallRightDown : new Sprite(0,3),
+    wallUpDownLeft : new Sprite(0,4),
+    wallUpDownRight : new Sprite(2,4), 
+    wallUpRight : new Sprite(0,5),
+    wallLeftRight : new Sprite(1,3),
+    wallDownLeft : new Sprite(2,3),
+    wallLeftUp :new Sprite(2,5),
+    floor : new Sprite(3,4)
 }
 window.sprites = sprites;
 })()
