9ad50b6062cbdecd2b8d452ab89d0614edf092fb
StanislavMartynencko <ctys7777@gmail.com>
2017-06-01 22:35:45 +0300

    Объекты сцены

--- src/scripts/sprites.js
+++ src/scripts/sprites.js
@@ -12,7 +12,19 @@ var sprites = {
 	tailDown :new Sprite(3,1),
 	tailLeft :new Sprite(3,2),
 	tailRight :new Sprite(4,2),
-	headUp :new Sprite(5,1)
+	headUp :new Sprite(5,1),
+	headDown :new Sprite(5,2),
+	headLeft :new Sprite(4,0),
+	headRight :new Sprite(5,0),
+	food : new Sprite(4,1),
+	wallRightDown : new Sprite(0,3),
+	wallUpDownLeft : new Sprite(0,4),
+	wallUpDownRight : new Sprite(2,4), 
+	wallUpRight : new Sprite(0,5),
+	wallLeftRight : new Sprite(1,3),
+	wallDownLeft : new Sprite(2,3),
+	wallLeftUp :new Sprite(2,5),
+	floor : new Sprite(3,4)
 }
-
+window.sprites = sprites;
 })()
